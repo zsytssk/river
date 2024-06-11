f668d27f2692ca8e6be3377f9ffa886a248d08ef
shiyangzhang <shiyangzhang@whale.im>
2024-06-11 16:54:20 +0800

    [dev-zsy]

--- riverctl/printArgs.zig
+++ riverctl/printArgs.zig
@@ -1,28 +1,14 @@
 const std = @import("std");
 
 pub fn print(args: []const [*:0]const u8) !void {
+    // _ = args;
     const allocator = std.heap.page_allocator;
-
-    // 创建一个 ArrayList 来存储二维字节数组
-    var arrayList = std.ArrayList([]const u8).init(allocator);
-    defer arrayList.deinit();
-
-    // 向 ArrayList 添加元素
-    for (args) |arg| {
-        const new_arg: []const u8 = std.mem.span(arg);
-        try arrayList.append(new_arg);
-    }
-
-    var filterList = std.ArrayList([]const u8).init(allocator);
-    // 打印数组内容
-    for (arrayList.items) |item| {
-        // if (indexOf(item, "Super") != -1) {
-        //     continue;
-        // }
-        try filterList.append(item);
-    }
-    const res: []u8 = try concatArrayList(allocator, filterList, " ");
+    var res: []const u8 = "";
     defer allocator.free(res);
+    for (args) |arg| {
+        res = try std.fmt.allocPrint(allocator, "{s} {s}", .{ res, arg });
+    }
+
     if (indexOf(res, "Alt") != -1 and indexOf(res, "Super") == -1) {
         std.log.info("riverctl {s}", .{res});
     }
@@ -35,25 +21,4 @@ fn indexOf(s: []const u8, sub: []const u8) i32 {
         }
     }
     return -1;
-}
-
-pub fn concatArrayList(allocator: std.mem.Allocator, arr: std.ArrayList([]const u8), splitor: []const u8) ![]u8 {
-    var result: []u8 = "";
-    // 打印数组内容
-    for (arr.items, 0..) |slice, i| {
-        result = try concatStr(allocator, result, slice);
-        if (i != arr.items.len - 1) {
-            result = try concatStr(allocator, result, splitor);
-        }
-    }
-    return result;
-}
-
-pub fn concatStr(allocator: std.mem.Allocator, st1: []const u8, st2: []const u8) ![]u8 {
-    const len = st1.len + st2.len;
-    var result = try allocator.alloc(u8, len);
-    @memcpy(result[0..st1.len], st1);
-    @memcpy(result[st1.len..], st2);
-
-    return result;
 }
